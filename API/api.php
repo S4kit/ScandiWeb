e45123d9be6655a3a33362147c137363123a1492
SAID Benslimane <saidbenslimane@outlook.fr>
2023-07-05 03:08:13 +0100

    editedv2

--- API/api.php
+++ API/api.php
@@ -24,23 +24,25 @@ switch ($method) {
     case 'POST':
         $recievedJSon = file_get_contents("php://input");
         $decode = json_decode($recievedJSon, true);
+        if ($decode['operation'] === 'delete') {
 
-        $product = new Product($decode['sku'], $decode['name'], $decode['price'], $decode['type'], $decode['attribute']);
+            $response = $restAPI->Delete($decode['items']);
+            if ($response != true) {
+                http_response_code(500); // Internal Server Error
+            }
 
-        $response = $restAPI->saveProduct($product);
-        if ($response != true) {
-            http_response_code(500); // Internal Server Error
+        } else {
+
+            $product = new Product($decode['sku'], $decode['name'], $decode['price'], $decode['type'], $decode['attribute']);
+
+            $response = $restAPI->saveProduct($product);
+            if ($response != true) {
+                http_response_code(500); // Internal Server Error
+            }
         }
         break;
 
-    case 'DELETE':
-        $recievedJSon = file_get_contents("php://input");
-        $decode = json_decode($recievedJSon, true);
-        $response = $restAPI->Delete($decode);
-        if ($response != true) {
-            http_response_code(500); // Internal Server Error
-        }
-        break;
+
     default:
         http_response_code(405); // Method Not Allowed
         break;
